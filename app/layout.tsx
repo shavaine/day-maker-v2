27d7c6243ef3626093c0b55c66a973d38d8b674e
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-07-24 02:55:32 -0400

    Feat: Create AuthProvider Wrapper For Layout
    Allows for access to user session in all future components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navigation from "@/components/Navigation/Navigation";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import AuthProvider from "./AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navigation />
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navigation />
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
