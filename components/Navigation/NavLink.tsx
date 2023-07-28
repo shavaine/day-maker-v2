e647ef9b44dccc74e8dbb43d02a5953c379a33f8
shavaine <35738478+shavaine@users.noreply.github.com>
2023-07-28 14:36:04 -0400

    Refactor: Edit Navlink For Demo Routes
    Changes ensure that Demo route stays highlighted during sub route navigation.

--- components/Navigation/NavLink.tsx
+++ components/Navigation/NavLink.tsx
@@ -9,6 +9,21 @@ interface Props {
 
 export default function NavLink({ name, path }: Props) {
   const pathname = usePathname();
+  if (path === "/demo") {
+    return (
+      <Link
+        className={`${
+          pathname.includes(path)
+            ? "pb-2 border-b-4 border-[#471AA0FF] font-spaceMono font-bold text-[#471AA0FF] px-4"
+            : "pb-2 hover:border-b-4 hover:border-[#471AA0FF] font-spaceMono hover:font-bold text-[#471AA0FF] px-4"
+        }`}
+        href={path}
+      >
+        {name}
+      </Link>
+    );
+  }
+
   return (
     <Link
       className={`${
