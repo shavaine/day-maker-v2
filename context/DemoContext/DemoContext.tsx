1f81291176a0f8a003750c5f7f020c4c03eaa09f
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-07-29 01:25:16 -0400

    Feat: Create Context For Demo

---
+++ context/DemoContext/DemoContext.tsx
@@ -0,0 +1,12 @@
+import { createContext, Dispatch } from "react";
+import { initialState } from "./InitialData";
+import { InitialState } from "./Interfaces";
+import { ActionType } from "./Types";
+
+export const DemoContext = createContext<{
+  state: InitialState;
+  dispatch: Dispatch<ActionType>;
+}>({
+  state: initialState,
+  dispatch: () => {},
+});
