7643599572b1019b0e2bae85047cb8abda5e34e7
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-07-29 01:20:35 -0400

    Feat: Add ActionTypes For Actions
    To be used in reducer when dealing with actions.

---
+++ context/DemoContext/Types.ts
@@ -0,0 +1,6 @@
+import { Action } from "./Interfaces";
+
+export type ActionType =
+  | { type: "ADD_ACTION"; payload: Action }
+  | { type: "UPDATE_ACTION"; payload: Action }
+  | { type: "DELETE_ACTION"; payload: string } // payload is the actionId
