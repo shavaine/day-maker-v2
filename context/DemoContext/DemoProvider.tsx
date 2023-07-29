27d9ea59e0f82ac0273e667b71d1e45dc9612cf7
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-07-29 01:27:51 -0400

    Feat: Create Demo Context Provider
    To share the Demo State with all demo pages.

---
+++ context/DemoContext/DemoProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { FC, useReducer } from "react";
+import { initialState } from "./InitialData";
+import { Reducer } from "./Reducer";
+import { DemoContext } from "./DemoContext";
+
+interface DemoProviderProps {
+  children: React.ReactNode;
+}
+
+export const DemoProvider: FC<DemoProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  return (
+    <DemoContext.Provider value={{ state, dispatch }}>
+      {children}
+    </DemoContext.Provider>
+  );
+};
