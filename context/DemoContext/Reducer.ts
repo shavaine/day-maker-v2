9725aece856360ccf8802c209be7a5e1353da129
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-07-29 01:22:56 -0400

    Feat: Create Reducer With Action Cases
    To manipulate actions in future Demo Context.

---
+++ context/DemoContext/Reducer.ts
@@ -0,0 +1,24 @@
+import { InitialState } from "./Interfaces";
+import { ActionType } from "./Types";
+
+
+export function Reducer(state: InitialState, action: ActionType): InitialState {
+  switch (action.type) {
+    case "ADD_ACTION":
+      return { ...state, actions: [...state.actions, action.payload] };
+    case "UPDATE_ACTION":
+      return {
+        ...state,
+        actions: state.actions.map((currentAction) =>
+          currentAction.actionId === action.payload.actionId ? action.payload : currentAction
+        ),
+      };
+    case "DELETE_ACTION":
+      return {
+        ...state,
+        actions: state.actions.filter((currentAction) => currentAction.actionId !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
