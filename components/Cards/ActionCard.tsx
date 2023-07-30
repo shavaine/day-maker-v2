fde79e3ee499f41fe1b96a78662326fa7ec49431
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-07-30 07:36:50 -0400

    Feat: Add Action Card With Options And Details
    Displays action title with the option to delete or edit.

---
+++ components/Cards/ActionCard.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { DemoContext } from "@/context/DemoContext/DemoContext";
+import { Action } from "@/context/Interfaces";
+import { FC, useContext } from "react";
+import EditActionModal from "../Modals/EditActionModal";
+
+interface Props {
+  title: string;
+  actionId: string;
+}
+
+// Imported in ActionList Component
+const ActionCard: FC<Action> = ({
+  title,
+  actionId,
+}: Props): React.ReactNode => {
+  const { dispatch } = useContext(DemoContext);
+
+  const deleteAction = (actionId: string) => {
+    dispatch({ type: "DELETE_ACTION", payload: actionId });
+  };
+
+  return (
+    <div className="flex flex-col justify-between bg-white border shadow w-96 h-40 pt-3 break-all overflow-auto font-spaceMon rounded-md">
+      <p className="text-center font-bold text-mainColor text-lg">{title}</p>
+      <div className="flex flex-row w-full">
+        <EditActionModal title={title} actionId={actionId} />
+        <button
+          className="bg-red-200 text-mainColor font-bold w-1/2 grow hover:opacity-80 p-2"
+          onClick={() => deleteAction(actionId)}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ActionCard;
