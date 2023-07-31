4825f4023f8b90ef4de07c250fbadf5a8432576c
shavaine <35738478+shavaine@users.noreply.github.com>
2023-07-31 12:19:02 -0400

    Feat: Add Create Template Route & Page

--- app/demo/templates/create/page.tsx
+++ app/demo/templates/create/page.tsx
@@ -1,18 +1,16 @@
-import TemplateList from "@/components/List/TemplateList";
-import Link from "next/link";
+import CreateTemplateCard from "@/components/Cards/CreateTemplateCard";
 
 export default function Templates() {
   return (
     <div className="max-h-screen overflow-auto">
       <div className="p-10 flex flex-row justify-between ">
         <h1 className=" text-secondaryColor font-workSans font-bold text-4xl">
-          Templates
+          Create Template
         </h1>
-        <Link href="/demo/templates/create">
-          <h4 className="text-lg text-gray-500">Create Template</h4>
-        </Link>
       </div>
-      <TemplateList />
+      <div className="p-10">
+        <CreateTemplateCard />
+      </div>
     </div>
   );
 }
