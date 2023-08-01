ea62d1df6d39505161d6aa61298ffe677ad09e38
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-08-01 17:51:06 -0400

    Feat: Add Template Table To Display List of Tasks

--- app/demo/templates/view/[tempId]/page.tsx
+++ app/demo/templates/view/[tempId]/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 
+import TemplateTable from "@/components/Tables/TemplateTable";
 import { DemoContext } from "@/context/DemoContext/DemoContext";
 import { getTemplateNameById } from "@/lib/helpers";
-import { useContext } from "react";
+import { FC, useContext } from "react";
 
 interface Props {
   params: { tempId: string };
 }
 
-const TemplateView = ({ params }: Props) => {
+const TemplateView: FC<Props> = ({ params }) => {
   const { state } = useContext(DemoContext);
 
   return (
-    <div className="flex flex-col bg-white p-4 rounded-md border shadow">
+    <div className="flex flex-col gap-y-5 bg-white p-4 rounded-md border shadow">
       <h1 className=" text-secondaryColor font-workSans font-bold text-4xl text-center">
         {getTemplateNameById(params.tempId, state.templates)}
       </h1>
+      <TemplateTable tempId={params.tempId} />
     </div>
   );
 };
---
+++ components/Tables/TemplateTable.tsx
@@ -0,0 +1,26 @@
+import { DemoContext } from "@/context/DemoContext/DemoContext";
+import { FC, useContext } from "react";
+import TaskTableRow from "./TaskTableRow";
+
+interface Props {
+  tempId: string;
+}
+const TemplateTable: FC<Props> = ({ tempId }) => {
+  const { state } = useContext(DemoContext);
+  return (
+    <table>
+      <tr className="bg-gray-100">
+        <th>Start Time</th>
+        <th>End Time</th>
+        <th>Action</th>
+        <th>Notes</th>
+      </tr>
+      {state.tasks
+        .filter((task) => task.templateId === tempId)
+        .map((task, index) => (
+          <TaskTableRow key={task.taskId} task={task} index={index} />
+        ))}
+    </table>
+  );
+};
+export default TemplateTable;
