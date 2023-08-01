c9ad73ad4d0dc8c25e5a3deeeec785b700719965
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-08-01 16:40:12 -0400

    Feat: Add GetTemplateNameByID  Helper Function

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,10 +1,17 @@
-import { Action, Task } from "@/context/Interfaces";
+import { Action, Task, Template } from "@/context/Interfaces";
 
   export const getActionTitleById = (actionId: string, actions: Action[]) => {
     const actionName = actions.find(
       (action) => action.actionId === actionId
     )?.title;
     return actionName;
+  };
+
+  export const getTemplateNameById = (templateId: string, templates: Template[]) => {
+    const templateName = templates.find(
+      (template) => template.templateId === templateId
+    )?.name;
+    return templateName;
   };
 
     export const applyTemplateId = (tempId: string, tasks: Task[]) => {
