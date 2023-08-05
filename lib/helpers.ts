92ec183da5b60e7af6da976085ce583289370683
shavaine <35738478+shavaine@users.noreply.github.com>
2023-08-04 22:19:02 -0400

    Feat: Add Helper Function To Format Time
    Formats time from minutes to HH:MM String.

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -20,3 +20,11 @@ import { Action, Task, Template } from "@/context/Interfaces";
       return task;
     });
   };
+
+  export const formatTime = (minutes: number): string => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${String(hours).padStart(2, "0")}:${String(
+      remainingMinutes
+    ).padStart(2, "0")}`;
+  }
