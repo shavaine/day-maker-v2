3da8a59d81da917adcaed942fc8a3d6eb3068ecd
shavaine <35738478+shavaine@users.noreply.github.com>
2023-08-06 01:23:53 -0400

    Feat: Add Schedule To Schedule Page & Mock Options

--- app/demo/schedule/page.tsx
+++ app/demo/schedule/page.tsx
@@ -1,9 +1,20 @@
+import ScheduleCard from "@/components/Cards/ScheduleCard";
+
 export default function Schedule() {
   return (
-    <div>
-      <h1 className="p-10 flex flex-row text-secondaryColor font-workSans font-bold text-4xl">
-        Schedule
-      </h1>
+    <div className="max-h-screen overflow-auto">
+      <div className="p-10 flex flex-row justify-between ">
+        <h1 className=" text-secondaryColor font-workSans font-bold text-4xl">
+          Schedule
+        </h1>
+        <div className="flex flex-row gap-x-6">
+          <h4 className="text-lg text-gray-500">Change Template</h4>
+          <h4 className="text-lg text-gray-500">Delete Template</h4>
+        </div>
+      </div>
+      <div className="p-10">
+        <ScheduleCard />
+      </div>
     </div>
   );
 }
