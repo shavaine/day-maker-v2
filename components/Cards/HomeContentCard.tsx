bd79a1f3f6f12810a557f92849cba0e224c2fabf
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-08-23 00:48:49 -0400

    Refactor: Change Home Content Text & Color

--- components/Cards/HomeContentCard.tsx
+++ components/Cards/HomeContentCard.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function HomeContentCard() {
   return (
     <div className="container py-20 bg-white rounded-lg flex flex-col font-workSans text-center drop-shadow-2xl border">
-      <h1 className="font-bold  text-4xl text-purple-950 mb-16">How To Use</h1>
+      <h1 className="font-bold  text-5xl text-purple-950 mb-16">How To Use</h1>
       <div className="flex flex-col gap-y-6">
         <div className="flex flex-row justify-between">
-          <h3 className="self-center text-mainColor text-2xl text-start w-[60%] font-bold">
+          <h3 className="self-center text-purple-700 text-2xl text-start w-[60%] font-bold">
             1. Create reusable Actions that can be assigned to task
           </h3>
           <Image
@@ -18,8 +18,8 @@ export default function HomeContentCard() {
           />
         </div>
         <div className="flex flex-row justify-between">
-          <h3 className="self-center text-mainColor text-2xl text-start w-[60%] font-bold">
-            2. Create Templates and add Tasks
+          <h3 className="self-center text-purple-700 text-2xl text-start w-[60%] font-bold">
+            2. Create Templates and add tasks using your reusable actions
           </h3>
           <Image
             src="/assets/placeholder-wide.png"
@@ -30,8 +30,8 @@ export default function HomeContentCard() {
           />
         </div>
         <div className="flex flex-row justify-between">
-          <h3 className="self-center text-mainColor text-2xl text-start w-[60%] font-bold">
-            3. Create Schedules by selecting a date and template
+          <h3 className="self-center text-purple-700 text-2xl text-start w-[60%] font-bold">
+            3. Create Schedules by selecting a date and adding a template
           </h3>
           <Image
             src="/assets/placeholder-wide.png"
