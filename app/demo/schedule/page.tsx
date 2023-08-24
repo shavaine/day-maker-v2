3227396b83e675b438a770587ea09566681573af
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-08-24 11:05:01 -0400

    Feat: Add Create Schedule Page & Route

--- app/demo/schedule/page.tsx
+++ app/demo/schedule/page.tsx
@@ -1,11 +1,16 @@
 import ScheduleCard from "@/components/Cards/ScheduleCard";
+import Link from "next/link";
 
 export default function Schedule() {
   return (
     <div className="max-h-screen overflow-auto">
       <div className="flex flex-row justify-between ">
         <h1 className="pageTitle">Schedule</h1>
-        <h4 className="text-lg text-gray-500 self-center">Create Schedule</h4>
+        <Link href="/demo/schedule/create">
+          <h4 className="text-lg text-gray-500 self-center hover:font-bold mt-5">
+            Create Schedule
+          </h4>
+        </Link>
       </div>
       <ScheduleCard />
     </div>
