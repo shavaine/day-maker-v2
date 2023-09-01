b2cc954dd823158e542c9e899a22da339c2776ce
shavaine <35738478+shavaine@users.noreply.github.com>
2023-09-01 08:32:28 -0400

    Perf: Return JSON Message If No Session For API

--- app/api/actions/create/route.ts
+++ app/api/actions/create/route.ts
@@ -5,6 +5,12 @@ import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(req: Request) {
     const session = await getServerSession(authOptions);
+    
+    // Returns JSON message if API is accessed without authentication 
+    if (!session) {
+        return NextResponse.json({message: "Must be logged in to use API"});
+    }
+
     const user = await prisma.user.findFirst({
         where: { email: session?.user?.email! },
     });
