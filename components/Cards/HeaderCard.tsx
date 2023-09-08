e56d81e9eb3995c435520413fff426b57418f11a
shavaine <35738478+shavaine@users.noreply.github.com>
2023-09-07 23:58:29 -0400

    Style: Resize Home Header Card For Mobile

--- components/Cards/HeaderCard.tsx
+++ components/Cards/HeaderCard.tsx
@@ -3,10 +3,12 @@ import { FaGithub } from "react-icons/fa";
 export default function HeaderCard() {
   return (
     <header className="container py-20 bg-white rounded-lg flex flex-col gap-y-3 text-center font-spaceMono shadow-xl">
-      <p className="font-bold uppercase text-[#171A1FFF]">
+      <p className="font-bold uppercase text-xl text-[#171A1FFF]">
         Welcome to Daymaker: Unleash your productivity potential!
       </p>
-      <h1 className="text-5xl text-mainColor">Scheduling Made Simple</h1>
+      <h1 className="text-xl lg:text-5xl text-mainColor">
+        Scheduling Made Simple
+      </h1>
       <p className="text-sm text-[#6F7787FF]">
         Effortlessly organize your schedule, supercharge your <br />{" "}
         productivity, and reclaim control of your day.
