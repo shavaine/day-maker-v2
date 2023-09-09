9d59d772c75756c19ee0e327a0f242b8c9d696c7
shavaine <35738478+shavaine@users.noreply.github.com>
2023-09-08 22:06:06 -0400

    Feat: Add Create Task & Template Mobile Button

--- app/dashboard/actions/page.tsx
+++ app/dashboard/actions/page.tsx
@@ -4,9 +4,12 @@ import CreateActionModal from "@/components/Modals/CreateActionModal";
 export default function Actions() {
   return (
     <div className="max-h-screen overflow-auto">
+      <h4 className="sm:hidden fixed bottom-20 right-1 z-50 bg-white rounded-full p-2 border">
+        <CreateActionModal />
+      </h4>
       <div className="flex flex-row justify-between ">
         <h1 className="pageTitle">Actions</h1>
-        <h4 className="text-lg text-gray-500 mt-5">
+        <h4 className="hidden sm:flex text-lg text-gray-500 mt-5">
           <CreateActionModal />
         </h4>
       </div>
