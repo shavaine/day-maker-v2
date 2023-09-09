7b3257ee6f192f5905bd7bbc8178e8c34baffdc9
shavaine <35738478+shavaine@users.noreply.github.com>
2023-09-08 22:08:07 -0400

    Feat: Add Privacy Policy Page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,125 @@
+const privacyPolicy = () => {
+  return (
+    <main>
+      <section className="bg-home-header py-32">
+        <header className="container py-20 bg-white rounded-lg flex flex-col gap-y-3 font-spaceMono shadow-xl">
+          <h1 className="text-5xl text-mainColor text-center">
+            Privacy Policy for DayMaker
+          </h1>
+          <p className="text-sm text-[#6F7787FF]">Effective Date: 2023-09-06</p>
+          <p className="text-sm text-[#6F7787FF]">
+            Welcome to DayMaker! We understand the importance of your privacy
+            and are committed to protecting your personal information. This
+            Privacy Policy outlines how DayMaker (&quot;we,&quot;
+            &quot;our,&quot; or &quot;us&quot;) collects, uses, discloses, and
+            safeguards your data when you use our scheduling app and related
+            services. By accessing or using DayMaker, you consent to the
+            practices described in this Privacy Policy. If you do not agree with
+            our practices, please do not use our services.
+          </p>
+          <p className="font-bold uppercase text-[#171A1FFF]">
+            Information We Collect
+          </p>
+          <ol className="text-sm text-[#6F7787FF]">
+            <li>
+              1. User Account Information: When you create an account with
+              DayMaker, we collect personal information such as your name and
+              email address to establish and manage your account.
+            </li>
+            <li>
+              2. Actions, Templates, and Schedules: DayMaker allows you to
+              create reusable Actions, Templates, and Schedules. These may
+              include descriptions and other task-related data that you input.
+            </li>
+            <li>
+              3. Usage Information: We collect information about how you
+              interact with our app, including your activity, usage patterns,
+              and device information (e.g., browser type, IP address).
+            </li>
+            <li>
+              4. Communication Data: If you contact us for support or other
+              purposes, we may collect and store the communication, including
+              your email address.
+            </li>
+          </ol>
+          <p className="font-bold uppercase text-[#171A1FFF]">
+            How We Use Your Information
+          </p>
+          <ol className="text-sm text-[#6F7787FF]">
+            <li>
+              1. To Provide Services: We use your personal information to
+              provide you with access to DayMaker`s features, including the
+              creation of Actions, Templates, and Schedules.
+            </li>
+            <li>
+              2. To Communicate: We may use your email address to send you
+              important updates, notifications, and information related to your
+              account or our services.
+            </li>
+            <li>
+              3. To Improve Our Services: We analyze usage patterns and feedback
+              to enhance our app`s functionality, user experience, and features.
+            </li>
+          </ol>
+          <p className="font-bold uppercase text-[#171A1FFF]">
+            Data Sharing and Disclosure
+          </p>
+          <p className="text-sm text-[#6F7787FF]">
+            We do not sell your personal information to third parties. However,
+            we may share your information with:
+          </p>
+          <ol className="text-sm text-[#6F7787FF]">
+            <li>
+              Service Providers: We may engage third-party service providers to
+              assist us with app functionality, analytics, or support. These
+              providers may access and process your data but are contractually
+              bound to maintain its confidentiality.
+            </li>
+            <li>
+              Legal Requirements: We may disclose your information in response
+              to a legal request, court order, government investigation, or to
+              protect our rights, property, or safety.
+            </li>
+          </ol>
+          <p className="font-bold uppercase text-[#171A1FFF]">Security</p>
+          <p className="text-sm text-[#6F7787FF]">
+            We implement industry-standard security measures to protect your
+            data. However, no method of transmission over the internet or
+            electronic storage is entirely secure, and we cannot guarantee
+            absolute security.
+          </p>
+          <p className="font-bold uppercase text-[#171A1FFF]">Your Choices</p>
+          <p className="text-sm text-[#6F7787FF]">
+            You can access, update, or delete your personal information through
+            your DayMaker account settings. You may also opt-out of receiving
+            non-essential communications from us.
+          </p>
+          <p className="font-bold uppercase text-[#171A1FFF]">
+            Children`s Privacy
+          </p>
+          <p className="text-sm text-[#6F7787FF]">
+            DayMaker is not intended for use by individuals under the age of 13.
+            We do not knowingly collect personal information from children under
+            13. If you believe we have inadvertently collected such information,
+            please contact us to request its deletion.
+          </p>
+          <p className="font-bold uppercase text-[#171A1FFF]">
+            Changes to this Privacy Policy
+          </p>
+          <p className="text-sm text-[#6F7787FF]">
+            We may update this Privacy Policy to reflect changes in our data
+            practices or legal requirements. We will notify you of any
+            significant changes by posting a revised Privacy Policy on our
+            website.
+          </p>
+          <p className="font-bold uppercase text-[#171A1FFF]">Contact Us</p>
+          <p className="text-sm text-[#6F7787FF]">
+            If you have questions, concerns, or requests related to this Privacy
+            Policy or your data, please contact us at shavaine9@gmail.com.
+          </p>
+        </header>
+      </section>
+    </main>
+  );
+};
+export default privacyPolicy;
