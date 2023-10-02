ba89679955d0bd3d5db82bb1f4074914d573277c
Shavaine Brown <35738478+shavaine@users.noreply.github.com>
2023-10-02 05:43:10 -0400

    Refactor: Use Semantic Tags For Template Page

--- components/Layouts/TemplatePage.tsx
+++ components/Layouts/TemplatePage.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 
 const TemplatePage: FC = () => {
   return (
-    <div className="max-h-screen overflow-auto">
+    <section className="max-h-screen overflow-auto">
       <Link
         className="self-center sm:hidden fixed bottom-20 right-1 z-50 bg-white rounded-full p-2 border"
         href="templates/create"
@@ -13,16 +13,16 @@ const TemplatePage: FC = () => {
         <BiSolidAddToQueue className="sm:hidden text-3xl text-mainColor" />
       </Link>
 
-      <div className="flex flex-row justify-center sm:justify-between">
+      <section className="flex flex-row justify-center sm:justify-between">
         <h1 className="pageTitle">Templates</h1>
         <Link className="hidden sm:inline" href={`templates/create`}>
           <h4 className="text-lg text-gray-500 self-center hover:font-bold mt-5">
             Create Template
           </h4>
         </Link>
-      </div>
+      </section>
       <TemplateList />
-    </div>
+    </section>
   );
 };
 export default TemplatePage;
